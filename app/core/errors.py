8dec9f316817565ceee54374053563613ae1e452
subterraneanbob <4823336+subterraneanbob@users.noreply.github.com>
2026-04-08 03:48:35 +0300

    Implement auth usecase: register, login, get user profile

--- app/core/errors.py
+++ app/core/errors.py
@@ -13,4 +13,29 @@ class OpenRouterError(ApplicationBaseError):
     Ошибка при взаимодействии с сервисом OpenRouter.ai
     """
 
-    pass
+
+class UserConflictError(ApplicationBaseError):
+    """
+    Ошибка при регистрации, если пользователь с заданным e-mail уже существует.
+    """
+
+    def __init__(self, email: str):
+        super().__init__(f"User with the e-mail {email} already exists.")
+
+
+class UnauthorizedError(ApplicationBaseError):
+    """
+    Ошибка авторизации - неверный e-mail или пароль пользователя.
+    """
+
+    def __init__(self):
+        super().__init__("Invalid credentials.")
+
+
+class UserNotFound(ApplicationBaseError):
+    """
+    Ошибка - пользователь не найден.
+    """
+
+    def __init__(self, user_id: int):
+        super().__init__(f"User with the id {user_id} is not found.")
---
+++ app/usecases/auth.py
@@ -0,0 +1,86 @@
+from app.core.errors import UnauthorizedError, UserConflictError, UserNotFound
+from app.core.security import create_access_token, hash_password, verify_password
+from app.db.models import User
+from app.repositories.users import UsersOrmRepo
+
+
+class AuthUseCase:
+    """
+    Бизнес-логика регистрации и авторизации пользователей.
+    """
+
+    def __init__(self, users_repo: UsersOrmRepo):
+        self._users_repo = users_repo
+
+    async def register(self, email: str, plain_text_password: str):
+        """
+        Регистрирует нового пользователя.
+
+        Args:
+            email (str): e-mail пользователя.
+            plain_text_password (str): Пароль пользователя в открытом виде.
+
+        Raises:
+            UserConflictError: Если пользователь с указанным e-mail уже существует.
+        """
+        existing_user = await self._users_repo.get_user_by_email(email)
+
+        if existing_user:
+            raise UserConflictError(email)
+
+        new_user = User(
+            email=email,
+            password_hash=hash_password(plain_text_password),
+            role="user",
+        )
+
+        await self._users_repo.create(new_user)
+
+    async def login(self, email: str, plain_text_password: str) -> str:
+        """
+        Авторизует существующего пользователя и создаёт токен доступа.
+
+        Args:
+            email (str): e-mail пользователя.
+            plain_text_password (str): Пароль пользователя в открытом виде.
+
+        Raises:
+            UnauthorizedError: Если пользователь с указанным e-mail не существует
+                или указан неверный пароль.
+
+        Returns:
+            str: Токен доступа в виде строки.
+        """
+        existing_user = await self._users_repo.get_user_by_email(email)
+
+        # Если пользователь не найден, то используем None в качестве значения хеша при проверке.
+        # В этом случае проверка пароля никогда не будет успешной, но займёт примерно то же время,
+        # что и для верного пароля.
+        password_hash = existing_user.password_hash if existing_user else None
+
+        if not verify_password(plain_text_password, password_hash):
+            raise UnauthorizedError
+
+        return create_access_token(
+            subject=str(existing_user.id), role=existing_user.role
+        )
+
+    async def get_user_profile(self, user_id: int) -> User:
+        """
+        Получает профиль существующего пользователя.
+
+        Args:
+            user_id (int): Уникальный идентификатор пользователя.
+
+        Raises:
+            UserNotFound: Если пользователь с указанным идентификатором не найден.
+
+        Returns:
+            User: Профиль пользователя.
+        """
+        user = await self._users_repo.get_user_by_id(user_id)
+
+        if not user:
+            raise UserNotFound(user_id)
+
+        return user
